711f6f16b234bbd743acc47918e07431db84164c
MaksLevchenko <111285984+MaksLevchenko@users.noreply.github.com>
2024-11-23 00:08:31 +0300

    v 0.3.3

--- test_logic.py
+++ test_logic.py
@@ -15,16 +15,16 @@ class LogicTestCase(TestCase):
         real = f'книга Www успешно добавленна в базу с id 1'
         self.assertEqual(result, real)
 
+    def test_search_book(self):
+        real = [{"book_id": 1, "book_title": "Aww", "book_author": "Www. w. w.", "book_year": "1111", "book_status": "в наличии"}]
+        book = Book(id=1, title='Aww', author='Www. w. w.', year='1111')
+        book.add_book()
+        result = Book.search_book('aww')
+        self.assertEqual(result, real)
+
     def test_del_book(self):
         result = Book.del_book(1)
         self.assertEqual(result, True)
-
-    def test_search_book(self):
-        real = [{"book_id": 1, "book_title": "Www", "book_author": "Www. w. w.", "book_year": "1111", "book_status": "в наличии"}]
-        book = Book(id=1, title='Www', author='Www. w. w.', year='1111')
-        book.add_book()
-        result = Book.search_book('www')
-        self.assertEqual(result, real)
 
     def test_change_status_book(self):
         result = Book.change_status_book(1, 'выдана')
