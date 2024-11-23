9317d29f1a280f5b40b6bc81d8c926ca8eedb7e8
MaksLevchenko <111285984+MaksLevchenko@users.noreply.github.com>
2024-11-23 15:05:38 +0300

    Добавил документацию к тестам.

--- test_logic.py
+++ test_logic.py
@@ -9,6 +9,7 @@ from unittest.mock import patch
 class LogicTestCase(TestCase):
 
     def test_add_book(self):
+        """Тест добавления книги в базу"""
         book = Book(id=0, title='Www', author='Www. w. w.', year='1111')
         result = book.add_book()
 
@@ -16,6 +17,7 @@ class LogicTestCase(TestCase):
         self.assertEqual(result, real)
 
     def test_search_book(self):
+        """Тест поиска книги"""
         real = [{"book_id": 1, "book_title": "Aww", "book_author": "Www. w. w.", "book_year": "1111", "book_status": "в наличии"}]
         book = Book(id=1, title='Aww', author='Www. w. w.', year='1111')
         book.add_book()
@@ -23,13 +25,16 @@ class LogicTestCase(TestCase):
         self.assertEqual(result, real)
 
     def test_del_book(self):
+        """Тест удаления книги из базы"""
         result = Book.del_book(0)
         self.assertEqual(result, True)
 
     def test_change_status_book(self):
+        """Тест изменения статуса книги"""
         result = Book.change_status_book(0, 'выдана')
         self.assertEqual(result, True)
 
     def test_z_del(self):
+        """Очищение книг добавленных в тестах"""
         result = Book.del_book(1)
         self.assertEqual(result, True)
