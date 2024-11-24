00296c772f8f387fce5171949393d0d0b3ce310a
MaksLevchenko <111285984+MaksLevchenko@users.noreply.github.com>
2024-11-24 13:59:39 +0300

    v 0.3.3

--- test_logic.py
+++ test_logic.py
@@ -34,14 +34,6 @@ class LogicTestCase(TestCase):
         result = Book.change_status_book(0, 'выдана')
         self.assertEqual(result, True)
 
-    # def test_add_book_bad(self):
-    #     """Тест добавления книги в базу"""
-    #     book = Book(id=0, title='Www', author='Www. w. w.', year='1111')
-    #     result = book.add_book()
-
-    #     real = f'книга Www успешно добавленна в базу с id 0'
-    #     self.assertEqual(result, real)
-
     def test_search_book_bad(self):
         """Негативный тест поиска книги"""
         real = []
@@ -49,12 +41,12 @@ class LogicTestCase(TestCase):
         self.assertEqual(result, real)
 
     def test_del_book_bad(self):
-        """Негативный тест удаления книги из базы"""
+        """Негативный тест удаления книги из базы. Тест срабатывает при неправильном id книги"""
         result = Book.del_book(-1)
         self.assertEqual(result, False)
 
     def test_change_status_book_bad(self):
-        """Негативный тест изменения статуса книги"""
+        """Негативный тест изменения статуса книги. Тест срабатывает при неправильном id книги"""
         result = Book.change_status_book(-1, 'невыдана')
         self.assertEqual(result, False)
 
