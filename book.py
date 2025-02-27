3fc4f1d0d4fff71e04300bce9ad262a3f23c3379
MaksLevchenko <111285984+MaksLevchenko@users.noreply.github.com>
2025-02-27 14:30:43 +0300

    add tests

--- book.py
+++ book.py
@@ -1,3 +1,61 @@
+# import json
+# import os
+
+# from logic import add_book, del_book, search_book, change_status_book
+# from models import BookManager
+
+
+# # Главная функция программы
+# def main():
+#     funk = {
+#         '1': add_book,
+#         '2': del_book,
+#         '3': search_book,
+#         '4': BookManager.view_books,
+#         '5': change_status_book
+#         }
+
+#     # Вывод в консоль подсказки с доступными командами
+#     print('Что Вы хотите сделать? Введите один из вариантов от 1 до 5: ')
+#     print(' 1 - Добавить книгу.')
+#     print(' 2 - Удалить книгу.')
+#     print(' 3 - Поиск книги.')
+#     print(' 4 - Все книги.')
+#     print(' 5 - Изменить статус книги.')
+
+#     # Ввод команды
+#     action = input()
+
+#     # Проверка на то, что команда является числом от 1 до 5
+#     if action in funk:
+#         funk[action]()
+#     else:
+#         print('Я Вас не понял:')
+#         main()
+
+# # Создаём файл books.txt, если его ещё нет
+# if os.path.exists('books.txt'):
+#     data = {}
+#     data['books'] = []
+#     with open('books.txt', 'r', encoding='utf-8') as file:
+#         data_1 = file.readline()
+#     if not data_1:
+#         with open('books.txt', 'w', encoding='utf-8') as file:
+#             json.dump(data, file)
+# else:
+#     data = {}
+#     data['books'] = []
+#     with open('books.txt', 'a', encoding='utf-8') as file:
+#         json.dump(data, file)
+
+# if __name__ == "__main__":
+
+#     # Запускаем бесконечный цикл
+#     while True:
+#         # Вызываем функцию main
+#         main()
+
+
 import json
 import os
 
@@ -5,52 +63,56 @@ from logic import add_book, del_book, search_book, change_status_book
 from models import BookManager
 
 
+# Функция для завершения программы
+def exit_program():
+    print("Завершение работы...")
+    quit()
+
+
 # Главная функция программы
 def main():
     funk = {
-        '1': add_book, 
-        '2': del_book, 
-        '3': search_book, 
-        '4': BookManager.view_books, 
-        '5': change_status_book
-        }
+        1: add_book,
+        2: del_book,
+        3: search_book,
+        4: BookManager.view_books,
+        5: change_status_book,
+        6: exit_program,
+    }
 
     # Вывод в консоль подсказки с доступными командами
-    print('Что Вы хотите сделать? Введите один из вариантов от 1 до 5: ')
-    print(' 1 - Добавить книгу.')
-    print(' 2 - Удалить книгу.')
-    print(' 3 - Поиск книги.')
-    print(' 4 - Все книги.')
-    print(' 5 - Изменить статус книги.')
+    print("Что Вы хотите сделать? Введите один из вариантов от 1 до 6: ")
+    print(" 1 - Добавить книгу.")
+    print(" 2 - Удалить книгу.")
+    print(" 3 - Поиск книги.")
+    print(" 4 - Все книги.")
+    print(" 5 - Изменить статус книги.")
+    print(" 6 - Завершить программу.")
 
     # Ввод команды
-    action = input()
+    while True:
+        action = input()
+        if action.isdigit() and int(action) in funk:
+            funk[int(action)]()
+            break
+        else:
+            print("Неверная команда. Попробуйте снова.")
 
-    # Проверка на то, что команда является числом от 1 до 5
-    if action in funk:
-        funk[action]()
-    else:
-        print('Я Вас не понял:')
-        main()
 
-# Создаём файл books.txt, если его ещё нет
-if os.path.exists('books.txt'):
-    data = {}
-    data['books'] = []
-    with open('books.txt', 'r', encoding='utf-8') as file:
-        data_1 = file.readline()
-    if not data_1:
-        with open('books.txt', 'w', encoding='utf-8') as file:
-            json.dump(data, file)
+# Инициализация данных
+if os.path.exists("books.txt"):
+    data = {"books": []}
+    with open("books.txt", "r+", encoding="utf-8") as file:
+        try:
+            data = json.load(file)
+        except json.JSONDecodeError:
+            pass  # Файл пуст или повреждён, используем начальное значение data
 else:
-    data = {}
-    data['books'] = []
-    with open('books.txt', 'a', encoding='utf-8') as file:
+    data = {"books": []}
+    with open("books.txt", "w", encoding="utf-8") as file:
         json.dump(data, file)
 
-if __name__ == "__main__":
 
-    # Запускаем бесконечный цикл
-    while True:
-        # Вызываем функцию main
-        main()
+if __name__ == "__main__":
+    # Запуск основной функции
+    main()
--- models.py
+++ models.py
@@ -3,13 +3,12 @@ from other import info_print
 import json
 
 
-
 # Модель книги
-class Book():
+class Book:
 
-    status: str = 'в наличии'
+    status: str = "в наличии"
 
-    def __init__(self, title: str, author: str, year: str, id: int=2):
+    def __init__(self, title: str, author: str, year: str, id: int = 2):
 
         if id < 2:
             self.id = id
@@ -24,75 +23,82 @@ class Book():
         """Метод работает внутри класса и добавляет id книге"""
 
         # Открытие файла books.txt
-        with open('books.txt', 'r', encoding='utf-8') as file:
+        with open("books.txt", "r", encoding="utf-8") as file:
             data = json.load(file)
 
         # Присвоение книги id
-        if data['books']:
-            id = data['books'][-1]['book_id'] + 1
+        if data["books"]:
+            id = data["books"][-1]["book_id"] + 1
         else:
             id = 2
         return id
-        
+
+
 class BookManager(Book):
     """Класс управления книгами"""
-    
+
     def del_book(id: int) -> bool:
         """Метод удаляет книгу из файла books.txt по id"""
 
         # Открытие файла books.txt
-        with open('books.txt', 'r', encoding='utf-8') as file:
+        with open("books.txt", "r", encoding="utf-8") as file:
             data = json.load(file)
 
         flag = False
 
         # Поиск книги по id и её удаление
-        for book in data['books']:
-            if book['book_id'] == id:
-                data['books'].remove(book)
-                with open('books.txt', 'w', encoding='utf-8') as file:
+        for book in data["books"]:
+            if book["book_id"] == id:
+                data["books"].remove(book)
+                with open("books.txt", "w", encoding="utf-8") as file:
                     json.dump(data, file)
-                info_print(f'Книга с id {id} успешно удалена.')
+                info_print(f"Книга с id {id} успешно удалена.")
                 flag = True
         if flag:
             return True
         # Вывод ошибки, если книги с таким id нет в базе
         else:
             return False
-    
+
     def add_book(self) -> str:
         """Метод добавляет книгу в файл books.txt"""
 
         # Открытие файла books.txt
-        with open('books.txt', 'r', encoding='utf-8') as file:
+        with open("books.txt", "r", encoding="utf-8") as file:
             data = json.load(file)
 
-        data['books'].append({
-        'book_id': self.id,
-        'book_title': self.title,
-        'book_author': self.author,
-        'book_year': self.year,
-        'book_status': 'в наличии'
-        })
-        
+        data["books"].append(
+            {
+                "book_id": self.id,
+                "book_title": self.title,
+                "book_author": self.author,
+                "book_year": self.year,
+                "book_status": "в наличии",
+            }
+        )
+
         # Запись новой книги в файл books.txt
-        with open('books.txt', 'w', encoding='utf-8') as file:
+        with open("books.txt", "w", encoding="utf-8") as file:
             json.dump(data, file)
 
-        return f'книга {self.title} успешно добавленна в базу с id {self.id}'
-    
+        return f"книга {self.title} успешно добавленна в базу с id {self.id}"
+
     def search_book(search: str) -> list:
         """Метод поиска книги по названию, автору или году издания"""
         books = []
 
-         # Открытие файла books.txt
-        with open('books.txt', 'r', encoding='utf-8') as file:
+        # Открытие файла books.txt
+        with open("books.txt", "r", encoding="utf-8") as file:
             data = json.load(file)
 
-        if data['books']:
-            for book in data['books']:
+        if data["books"]:
+            for book in data["books"]:
                 # Непосредственно поиск книги по названию, автору или году издания книги
-                if book['book_title'].lower() == search or book['book_author'].lower() == search or book['book_year'] == search:
+                if (
+                    book["book_title"].lower() == search
+                    or book["book_author"].lower() == search
+                    or book["book_year"] == search
+                ):
                     books.append(book)
         return books
 
@@ -100,20 +106,20 @@ class BookManager(Book):
         """Метод находит книгу по id в файле books.txt меняет статус книги"""
 
         # Открытие файла books.txt
-        with open('books.txt', 'r', encoding='utf-8') as file:
+        with open("books.txt", "r", encoding="utf-8") as file:
             data = json.load(file)
 
         flag = False
 
-        if data['books']:
-            for book in data['books']:
+        if data["books"]:
+            for book in data["books"]:
 
                 # Непосредственно изменение статуса книги
-                if book['book_id'] == id:
-                    book['book_status'] = status.lower()
-                    with open('books.txt', 'w', encoding='utf-8') as file:
+                if book["book_id"] == id:
+                    book["book_status"] = status.lower()
+                    with open("books.txt", "w", encoding="utf-8") as file:
                         json.dump(data, file)
-            
+
                     flag = True
         if flag:
             return True
@@ -124,20 +130,18 @@ class BookManager(Book):
         """Метод выводит в консоль все книги"""
 
         # Открытие файла books.txt
-        with open('books.txt', 'r', encoding='utf-8') as file:
+        with open("books.txt", "r", encoding="utf-8") as file:
             data = json.load(file)
 
         # Непосредственно вывод в консоль всех книг из базы
-        if data['books']:
-            info_print('Вот все наши книги:')
-            for book in data['books']:
-                print(book['book_title'])
-                print(book['book_author'])
-                print(book['book_year'])
-                print(book['book_status'])
+        if data["books"]:
+            info_print("Вот все наши книги:")
+            for book in data["books"]:
+                print(book["book_title"])
+                print(book["book_author"])
+                print(book["book_year"])
+                print(book["book_status"])
                 print()
         # Вывод ошибки, если нет ни одной книги в базе
         else:
-            print('К сожалению в данный момент у нас нет ни одной книги(')
-
-    
+            print("К сожалению в данный момент у нас нет ни одной книги(")
